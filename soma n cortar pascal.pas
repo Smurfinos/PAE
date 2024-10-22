53dd22281817d1d74d137d88a1481c017e3f065b
Smurfinos <gargamelsmurfino@gmail.com>
2024-10-22 09:48:51 +0100

    Add files via upload

---
+++ soma n cortar pascal.pas
@@ -0,0 +1,21 @@
+﻿program MyProgram;
+uses Math, SysUtils;
+
+var
+    n, Cortar, Soma, i : integer;
+
+begin
+    WriteLn('introduza um numero');
+    Cortar := 0;
+    Soma := 0;
+    ReadLn(n);
+    For i := 1 to n do
+    begin
+        Soma := Trunc(Soma + n);
+        Cortar := Trunc(Cortar + 1);
+        WriteLn('introduza um numero ');
+        ReadLn(n)
+    end;
+    WriteLn('A Soma dos numeros introduzidos é :' + FloatToStr(Soma));
+    WriteLn(' Foram introduzidos ' + FloatToStr(Cortar) + ' Numeros')
+end.
